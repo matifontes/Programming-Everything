c00474a9c56eb89a75014331d6ac10e468f32e49
Matias Fontes <matifontes13@gmail.com>
2020-08-13 19:52:06 -0300

    Create 169.java

---
+++ Exercises - LeetCode/169.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=0;
+        int aux = nums.length / 2;
+        bool 
+        for(int i = 0; i<nums.length-1; i++){
+            int count = 0;
+            for(int j = i+1; j<nums.length; j++){
+                if (nums[i] == nums [j]){
+                    count ++;
+                }
+            }
+            if (count >= aux){
+                majority = nums[i];
+                i=nums.length;
+            }
+        }
+        return majority;
+    }
+}
