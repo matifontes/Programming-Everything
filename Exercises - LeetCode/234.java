c53027bbd6d1d432cbf30c9e57ba778141f05995
Matias Fontes <matifontes13@gmail.com>
2020-08-18 17:23:39 -0300

    Create 234.java

---
+++ Exercises - LeetCode/234.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head == null)    //Vacia retorno que es palindrome;
+            return true;
+
+        ListNode p = head;
+        ListNode prev = new ListNode(head.val);
+        
+        //Una lista nueva pero reverse;
+        
+        while(p.next != null){
+            ListNode temp = new ListNode(p.next.val);
+            temp.next = prev;
+            prev = temp;
+            p = p.next;
+        }
+
+        
+        //comparo nodo a nodo la original con la reverse;
+        
+        ListNode p1 = head;
+        ListNode p2 = prev;
+
+        while(p1!=null){
+            if(p1.val != p2.val)
+                return false;
+
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+
+    }
+}
