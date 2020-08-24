0ffbd6f6be2eefc9d3f608ccb759fe0144087891
Matias Fontes <matifontes13@gmail.com>
2020-08-24 16:54:58 -0300

    Create 665.java

---
+++ Exercises - LeetCode/665.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count=0;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]>nums[i+1]){
+                if(i>0){
+                    if(nums[i-1]<=nums[i+1])
+                        nums[i]=nums[i-1];
+                    else
+                        nums[i+1]=nums[i];
+                }
+                count++;
+                if(count>1)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
