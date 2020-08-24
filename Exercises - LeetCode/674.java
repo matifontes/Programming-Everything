5c846f9f062750634e6875d785f4e2a68893a753
Matias Fontes <matifontes13@gmail.com>
2020-08-24 17:10:50 -0300

    Create 674.java

---
+++ Exercises - LeetCode/674.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int count = 0;
+        int max = 0;
+        
+        for (int i=0; i<nums.length; i++){
+            if (i == 0){
+                count++;
+            }
+            else{
+                if (nums[i] > nums[i-1]){
+                    count++;
+                }else{
+                    max = Math.max(max,count);
+                    count = 1;
+                }
+            }
+            max = Math.max(max, count);
+        }
+        
+        return max;
+    }
+}
