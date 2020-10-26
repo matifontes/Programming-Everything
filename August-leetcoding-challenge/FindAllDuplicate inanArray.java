158d13d77384193c4dd8c317a3e548c42fb4b9f4
Matias Fontes <matifontes13@gmail.com>
2020-10-26 18:25:11 -0300

    Update FindAllDuplicate inanArray.java

--- August-leetcoding-challenge/FindAllDuplicate inanArray.java
+++ August-leetcoding-challenge/FindAllDuplicate inanArray.java
@@ -4,12 +4,12 @@ class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         int[] aux = new int[nums.length+1];
         List<Integer> res = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             aux[nums[i]]++;
         }
         
-        for(int i=1; i<aux.length;i++){
-            if(aux[i]>1){
+        for(int i = 1; i < aux.length; i++) {
+            if(aux[i] > 1) {
                 res.add(i);
             }
         }
