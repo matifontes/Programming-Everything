85a0f18993b16fe2d3b07f7afa86aa37295c7d08
Matias Fontes <matifontes13@gmail.com>
2020-10-26 18:09:40 -0300

    Update 448.java

--- Exercises - LeetCode/448.java
+++ Exercises - LeetCode/448.java
@@ -2,15 +2,15 @@ class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int[] aux = new int[nums.length+1];
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++) {
             aux[nums[i]]++;
         }
         
         
         List<Integer> res = new ArrayList<>();
         
-        for (int i=1;i<aux.length; i++){
-            if(aux[i]==0){
+        for (int i=1;i<aux.length; i++) {
+            if(aux[i]==0) {
                 res.add(i);
             }
         }
