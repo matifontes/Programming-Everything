4e836c6d87f88f9ce4ce34c7b702cde11065b3b4
Matias Fontes <matifontes13@gmail.com>
2020-10-26 18:10:39 -0300

    Update 876.java

--- Exercises - LeetCode/876.java
+++ Exercises - LeetCode/876.java
@@ -2,21 +2,22 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode aux = head;
         ListNode p = head;
-        int length =0;
-        while(aux.next != null){
+        int length = 0;
+        while(aux.next != null) {
             length++;
-            aux=aux.next;
+            aux = aux.next;
         }
         
-        int midAux=0;
+        int midAux = 0;
         
-        if(length % 2 == 0){
-            length=length/2;
-        }else{
-            length=length/2 +1;
+        if (length % 2 == 0) {
+            length = length / 2;
+        }
+        else {
+            length = length / 2 +1;
         }
         
-        for(int i=0; i<length; i++){
+        for(int i=0; i<length; i++) {
             p = p.next;
         }
         
