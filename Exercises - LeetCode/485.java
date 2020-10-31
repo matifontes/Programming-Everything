6d336bf3949b4bb5f786ac0e2546c2749c9c25b8
Matias Fontes <matifontes13@gmail.com>
2020-10-31 02:30:47 -0300

    Update 485.java

--- Exercises - LeetCode/485.java
+++ Exercises - LeetCode/485.java
@@ -5,15 +5,16 @@ class Solution {
         int count = 0;
         int max = 0;
         
-        for (int i=0; i<nums.length; i++){
-            if(nums[i] == 1){
-                count ++;
-            }else{
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;
+            }
+            else {
                 count = 0;
             }
             
-            if (count>max){
-                max=count;
+            if (count > max) {
+                max = count;
             }
         }
         
